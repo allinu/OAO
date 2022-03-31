cc62abf8006856562f924b9b42ce85bc20cfcd66
Liona <lionacc@163.com>
2022-03-31 13:38:40 +0800

    :ambulance: update domain

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/api": {
-                target: "http://127.0.0.1:3000",
+                target: "http://liona.opvs.tk:3000",
                 ws: true,
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
